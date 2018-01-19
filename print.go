42eefe1fa4fb86e92d61781ed497049ae31e64b8
Philipp Brüll <pb@simia.tech>
2018-01-19 11:58:57 +0100

    extended print

--- print.go
+++ print.go
@@ -1,32 +1,72 @@
 package env
 
 import (
+	"flag"
 	"fmt"
 	"io"
 )
 
-// PrintBash writes an example bash script with all registered environment field and it's values
-// to the provided io.Writer.
-func PrintBash(w io.Writer) {
+// PrintFlag returns a flag for the environment print.
+func PrintFlag() *string {
+	return flag.String("print", "", "print the environment in the given format. format can be 'short-bash', 'long-bash', 'short-dockerfile' and 'long-dockerfile'")
+}
+
+// Print prints the environment in the provided format.
+func Print(w io.Writer, format string) error {
 	eh := ErrorHandler
 	ErrorHandler = NullErrorHandler
+	defer func() {
+		ErrorHandler = eh
+	}()
+
+	switch format {
+	case "short-bash":
+		printShortBash(w)
+		return nil
+	case "long-bash":
+		printLongBash(w)
+		return nil
+	case "short-dockerfile":
+		printShortDockerfile(w)
+		return nil
+	case "long-dockerfile":
+		printLongDockerfile(w)
+		return nil
+	default:
+		return fmt.Errorf("unknown format '%s'. known values are 'short-bash', 'long-bash', 'short-dockerfile' and 'long-dockerfile'", format)
+	}
+}
+
+func printShortBash(w io.Writer) {
+	for _, field := range fields {
+		fmt.Fprintf(w, "%s=\"%s\"\n", field.Name(), field.Value())
+	}
+}
+
+func printLongBash(w io.Writer) {
 	for _, field := range fields {
 		fmt.Fprintf(w, "\n")
 		fmt.Fprintf(w, "# %s\n", field.Description())
 		fmt.Fprintf(w, "%s=\"%s\"\n", field.Name(), field.Value())
 	}
-	ErrorHandler = eh
 }
 
-// PrintDockerfile writes an example dockerfile with all registered environment field and it's values
-// to the provided io.Writer.
-func PrintDockerfile(w io.Writer) {
-	eh := ErrorHandler
-	ErrorHandler = NullErrorHandler
+func printShortDockerfile(w io.Writer) {
+	for index, field := range fields {
+		if index == 0 {
+			fmt.Fprintf(w, "ENV ")
+		} else {
+			fmt.Fprintf(w, " \\\n    ")
+		}
+		fmt.Fprintf(w, "%s=\"%s\"", field.Name(), field.Value())
+	}
+	fmt.Fprintln(w)
+}
+
+func printLongDockerfile(w io.Writer) {
 	for _, field := range fields {
-		fmt.Fprintf(w, "\n")
+		fmt.Fprintln(w)
 		fmt.Fprintf(w, "# %s\n", field.Description())
 		fmt.Fprintf(w, "ENV %s %s\n", field.Name(), field.Value())
 	}
-	ErrorHandler = eh
 }
--- print_test.go
+++ print_test.go
@@ -4,36 +4,32 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/simia-tech/env"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/simia-tech/env"
 )
 
-func TestPrintBashWithGeneratedDescription(t *testing.T) {
+func TestPrint(t *testing.T) {
 	env.Clear()
 	env.String("TEST_ONE", "default", env.Required(), env.AllowedValues("one", "two", "three"))
 
-	buffer := &bytes.Buffer{}
-	env.PrintBash(buffer)
+	tcs := []struct {
+		name           string
+		format         string
+		expectedOutput string
+	}{
+		{"ShortBash", "short-bash", "TEST_ONE=\"default\"\n"},
+		{"LongBash", "long-bash", "\n# String field. Required field. Allowed values are 'one', 'two' and 'three'. The default value is 'default'.\nTEST_ONE=\"default\"\n"},
+		{"ShortDockerfile", "short-dockerfile", "ENV TEST_ONE=\"default\"\n"},
+		{"LongDockerfile", "long-dockerfile", "\n# String field. Required field. Allowed values are 'one', 'two' and 'three'. The default value is 'default'.\nENV TEST_ONE default\n"},
+	}
 
-	assert.Equal(t, "\n# String field. Required field. Allowed values are 'one', 'two' and 'three'. The default value is 'default'.\nTEST_ONE=\"default\"\n", buffer.String())
-}
-
-func TestPrintBashWithCustomDescription(t *testing.T) {
-	env.Clear()
-	env.String("TEST_ONE", "default", env.Description("Test field."))
-
-	buffer := &bytes.Buffer{}
-	env.PrintBash(buffer)
-
-	assert.Equal(t, "\n# Test field.\nTEST_ONE=\"default\"\n", buffer.String())
-}
-
-func TestPrintDockerfileWithGeneratedDescription(t *testing.T) {
-	env.Clear()
-	env.String("TEST_ONE", "default", env.Required(), env.AllowedValues("one", "two", "three"))
-
-	buffer := &bytes.Buffer{}
-	env.PrintDockerfile(buffer)
-
-	assert.Equal(t, "\n# String field. Required field. Allowed values are 'one', 'two' and 'three'. The default value is 'default'.\nENV TEST_ONE default\n", buffer.String())
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			buffer := &bytes.Buffer{}
+			require.NoError(t, env.Print(buffer, tc.format))
+			assert.Equal(t, tc.expectedOutput, buffer.String())
+		})
+	}
 }
